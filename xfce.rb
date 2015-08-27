5a6222147d780b350c4a3c6c060dd1562a945bbf
andrey <lamzin.an@gmail.com>
2015-08-27 18:00:55 +0300

    Добавлен хедп по xfce

---
+++ xfce.rb
@@ -0,0 +1,7 @@
+module Xfce
+    class Keyboard
+        def howCreateWindowMenuShortcut
+            #Menu -> Настройки -> Внешний вид -> Вкладка Настройки -> Чек "Включить редактируемые комбинации клавишь"
+        end
+    end
+end
