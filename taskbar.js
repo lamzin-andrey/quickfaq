0df8f15e095b37602b98cc7eb03e6b1913db9d7e
andrey <lamzin.an@gmail.com>
2015-09-04 19:22:44 +0300

    Stgart add menu

--- taskbar.js
+++ taskbar.js
@@ -8,7 +8,7 @@
 // ==/UserScript==
 
 (function LATaskbar(){
-  var B, Apps;
+  var W = window, D = document, B, Apps, Bar = null, INFO = {};
   window.onload = function() {
      init();
      addBar();
@@ -16,11 +16,54 @@
   
   //append taskbar on page
   function addBar() {
-   
+    taskBarView();
+    appMenuView();
+    taskBarLogic();
+  }
+  //TODO STOP HERE 
+  function appMenuView() {
+    var htmlItem  = '<li><img src="{SRC}"/>{TEXT}</li>',
+        html = '<div><input type="text"></div>',
+        view  = ce('ul'),
+        style = '';
+    Bar.list = view;
+  }
+  //set click handler
+  function taskBarLogic() {
+    Bar.onclick = function() {
+      toggleAppMenu();
+    }
+  }
+  //@see name )
+  function toggleAppMenu() {
+    showMenu(INFO.menuIsShow  ? 1 : 0);
+  }
+  function showMenu(isVisible) {
+    INFO.menuIsShow = isVisible;
+  }
+  //draw taskbar
+  function taskBarView() {
+    var L = localStorage, l = L.getItem('LATbarL'), t = L.getItem('LATbarT');
+    t = t ? t : getViewport().h - 10;
+    l = l ? l : getViewport().w - 100;
+    var style = "position:fixed; z-index:30000; left:" + l + "px; top:" + t + "px;";
+    style += 'background-color:#6DC6ED; width:60px; height:60px';
+    d = ce('div', B, 'LATbarL');
+    d.style = style;
+    Bar = d;
   }
   //init global vars
   function init() {
     B = document.body;
+  }
+  //====================quickllib====================
+  function ce(tag, parent, id) {
+    if (!parent) {
+      parent = document.body;
+    }
+    var e = document.createElement(tag);
+    parent.appendChild(e);
+    return e;
   }
   function e(i) {
     if (i && i.tagName) {
@@ -45,5 +88,20 @@
       return o.length
     }
     return false;
+  }
+  
+  function getViewport() {
+    var w = W.innerWidth, h = W.innerHeight;
+    if (!w && D.documentElement && D.documentElement.clientWidth) {
+      w = D.documentElement.clientWidth;
+    } else if (!w) {
+      w = D.getElementsByTagName('body')[0].clientWidth;
+    }
+    if (!h && D.documentElement && D.documentElement.clientHeight) {
+      h = D.documentElement.clientHeight;
+    } else if (!h) {
+      h = D.getElementsByTagName('body')[0].clientHeight;
+    }
+    return {w:w, h:h};
   }
 })()
