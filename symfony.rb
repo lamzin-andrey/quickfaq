304461637cd4572a8da5b60a10ad36ea91da556a
alamzin <lamzin80@mail.ru>
2015-12-07 09:21:35 +0300

    symfony.rb

---
+++ symfony.rb
@@ -0,0 +1,76 @@
+module Console
+  def createBundle #Как сгенерить бандл генерация бандла 
+    #php app/console generate:bundle --namespace=Acme/HelloBundle
+  end
+  def createDatabase
+    #write params in app/config/parameters.yml
+    #php app/console doctrine:database:create
+  end
+  def createTable
+    #php app/console doctrine:generate:entity --entity="AcmeHelloBundle:Product" 
+    #> --fields="name:string(255) description:text price:float"
+    
+    #php app/console doctrine:schema:update --force
+  end
+  def clearCache
+    #php app/console cache:clear --env=prod --no-debug
+    #php app/console cache:clear --env=dev --no-debug
+  end
+  def migration
+    #composer require doctrine/doctrine-migrations-bundle "^1.0"
+    
+    #add string in app/AppKernel.php
+    #new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
+    
+    #add config in app/config/parameters.yml
+    #doctrine_migrations:
+    #dir_name: "%kernel.root_dir%/DoctrineMigrations"
+    #namespace: Application\Migrations
+    #table_name: migration_versions
+    #name: Application Migrations
+  
+    #php app/console doctrine:migrations:status --show-versions
+    #php app/console doctrine:migrations:migrate 20151202122751
+  end
+end
+
+module Configuration
+  def mainConfigName
+    #app/config/parameters.yml
+  end
+end
+
+module Controller
+  def orm
+    #write 
+    #$em = $this->getDoctrine()->getEntityManager();
+	#$em->persist($product);
+	#$em->flush();
+	
+	#get
+	#$repository = $this->getDoctrine()->getRepository("AcmeHelloBundle:Product");
+  end
+  
+  def request
+    #$request = $this->getRequest();
+    # ->has(); (?) -> get(); (?)  -> set(); (?)
+    #$attr = $request->attributes
+    # ->has();  -> get();   -> set(); (?)
+  end
+  
+  def session
+    #$session = $this->getRequest()->getSession();
+    
+    # ->has(); -> get()  -> set() (?)
+  end
+  
+  def AuthUser
+    #Подключение SecurityContext
+	#use Symfony\Component\Security\Core\SecurityContext;
+	
+	#Как проверить, была ли ошибка авторизации
+	#$request->attributes->has(SecurityContext::AUTHENTICATION_ERROR);
+	#$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+
+  end
+end
