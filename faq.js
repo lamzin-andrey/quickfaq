899203dca6b14a5a6a64e93aa72a88c50a8069c4
andrey <andrey@andrey-NULL>
2016-03-10 18:00:45 +0300

    datetime

--- faq.js
+++ faq.js
@@ -45,13 +45,14 @@ function datetime(rus) {
 		}
 		return n;
 	}
-	var dt = new Date();
-	day = _null(dt.getDate());
-	month = _null(dt.getMonth() + 1);
-	month = _null(dt.getMonth() + 1);
-	hours = _null(dt.getHours());
-	min = _null(dt.getMinutes());
-	sec = _null(dt.getSeconds());
+	var dt = new Date(),
+	day = _null(dt.getDate()),
+	month = _null(dt.getMonth() + 1),
+	month = _null(dt.getMonth() + 1),
+	hours = _null(dt.getHours()),
+	min = _null(dt.getMinutes()),
+	sec = _null(dt.getSeconds()),
+	year = dt.getFullYear();
 	var sDate = day + '.' + month + '.' + year + ' ' + hours + ':' + min + ':' + sec;
 	if (!rus) {
 		sDate = year + '-' + month + '-' + day + ' ' + hours + ':' + min + ':' + sec;
