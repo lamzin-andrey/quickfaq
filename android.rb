feac0cdfb5c904e30bc74fae151a890e9bb166a8
alamzin <lamzin80@mail.ru>
2016-04-22 21:47:11 +0300

    autocommit

---
+++ android.rb
@@ -0,0 +1,12 @@
+#nil is "object"
+#nil == nil #true
+#reciever - object, his method was called
+#obj.method!  - "bang" or "mutator" method
+module Apk
+	class SignedApk					#example child class see MyException
+		#console
+		#jarsigner -keystore mykeys.keystore -storepass spassword -keypass kpassword1 Package1.apk mykey1
+		#jarsigner -keystore /home/andrey/hdata/programs/dev/flipcat/lamzin.keystore -storepass 123456 -keypass 123456 /home/andrey/hdata/programs/dev/flipcat/android_flipcat_2_1/out/production/flipcat/flipcat.apk lamzinAlias
+
+	end
+end
