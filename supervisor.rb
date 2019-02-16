ff51cfe4ef27ecd7114a23df13084ce04fd5a63d
Andrey Lamzin <lamzin.an@gmail.com>
2019-02-16 12:49:40 +0300

    Autocommit

---
+++ supervisor.rb
@@ -0,0 +1,8 @@
+module Supervisor
+	class DeleteCommand
+		#u 16.04 server
+		#конфиги супервизора лежат в дире /etc/supervisor/conf.d
+		#удалите и перезапустите супервизор командой
+		#systemctl restart supervisor
+	end
+end
