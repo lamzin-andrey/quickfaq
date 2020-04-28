83df357697b761cf934e3f5ec8cfa60501498710
andrey <lamzin.an@gmail.com>
2020-04-28 10:15:58 +0300

    Autocommit

--- bootstrap4.rb
+++ bootstrap4.rb
@@ -2,9 +2,9 @@ module Bootstrap
     class Collapse
         def example
             #<p>
-			#	<a href="#oldVersions" data-toggle="collapse" >Более ранние версии FastXampp</a>
+				#<a href="#oldVersions" data-toggle="collapse" >Более ранние версии FastXampp</a>
 			#</p>
-			#
+			
 			#<div id="oldVersions" class="collapse">...</div>
         end
     end
