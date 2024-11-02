b2911c7049b22b761311068fc14f2350b9d6aaf8
andrey <lamzin.an@rosfines.ru>
2024-11-02 16:48:01 +0300

    Autocommit

--- bash.rb
+++ bash.rb
@@ -3,3 +3,30 @@ module FileSystem
 		du -hs /path/to/dir
 	end
 end
+
+module ssh
+	class screen
+		#~ На примере переноса данных с лжного хоста на другой.
+		#~ screen -S move_api_data_2024-05
+		#~ Выйти:
+		#~ Вы можете легко выйти из screen - сессии набрав:
+		#~ Ctrl+a и d
+
+		#~ Вернуться
+		#~ screen -r
+
+		#~ Если у вас запущено больше чем одна screen - сессия, то после ключа r нужно указать ее ID. Узнать его просто с помощью команды:
+
+		#~ screen -ls
+		#~ There are screens on:
+				#~ 32328.move_api_data_2024-05  (Detached)
+				#~ 32482.wiki.merionet.ru_is_one_love      (Detached)
+		#~ 2 Sockets in /var/run/screen/S-root.
+
+		#~ screen -r 32328
+		
+		# Kill screen
+		# screen -X -S [session # you want to kill] quit
+	end
+end
+
--- swagger.rb
+++ swagger.rb
@@ -1,91 +1,102 @@
 module Examples
-	class Component
-		def Object
-			### start example
-			#Power:
-			  #type: object
-			  #description: Описание объекта
-			  #properties:
-				#id:# Имя свойства
-				  #type: integer
-				  #description: id сущности
-				#name:# Имя свойства
-				  #type: integer
-				  #example: 180
-				  #description: Описание свойства
-			### end example
-		
-			### adsress in tree (include first '#')
-			#/components/schemas/Power
-			
-			### example
-			Path.GetWithArray
-			### end example
-		
-		end
-	end
-	
-	class Path
-		def GetWithArray
-		  ### start example
-		  #/catalog/product/{productId}:
-			#get:
-			  #summary: Метод возвращает список продуктов
-			  #parameters:
-				#- name: productId
-				  #in: path
-				  #required: true
-				  #schema:
-					#type: integer
-			  #responses:
-				#'200':
-				  #description: массив продуктов с мощностями и их id
-				  #content:
-					#application/json:
-					  #schema:
-						#type: object
-						#properties:
-						  #items:
-							#type: array
-							#items:
-							  #allOf:
-								#- $ref: '#/components/schemas/Power'
-				#'404':
-				  #$ref: '#/components/responses/NotFoundErrorResponse'
-			  #tags:
-				#- Catalog
-		  ### end example
-		end
-		end
-		
-		def getWithArgumentInPath
-			Path.GetWithArray
-		end
-	end
+    class Component
+        def Object
+            ### start example
+            #Power:
+              #type: object
+              #description: Описание объекта
+              #properties:
+                #id:# Имя свойства (здесь пусто после двоеточия)
+                  #type: integer
+                  #description: id сущности
+                #name:# Имя свойства (здесь пусто после двоеточия)
+                  #type: integer
+                  #example: 180
+                  #description: Описание свойства
+            ### end example
+        
+            ### adsress in tree (include first '#')
+            #/components/schemas/Power
+            
+            ### example
+            Path.GetWithArray
+            ### end example
+        
+        end
+    end
+    
+    class Path
+        def GetWithArray
+          ### start example
+          #/catalog/product/{productId}:
+            #get:
+              #summary: Метод возвращает список продуктов
+              #parameters:
+                #- name: productId
+                  #in: path
+                  #required: true
+                  #schema:
+                    #type: integer
+              #responses:
+                #'200':
+                  #description: массив продуктов с мощностями и их id
+                  #content:
+                    #application/json:
+                      #schema:
+                        #type: object
+                        #properties:
+                          #items:
+                            #type: array
+                            #items:
+                              #allOf:
+                                #- $ref: '#/components/schemas/Power'
+                #'404':
+                  #$ref: '#/components/responses/NotFoundErrorResponse'
+              #tags:
+                #- Catalog
+          ### end example
+        end
+        end
+        
+        def getWithArgumentInPath
+            Path.GetWithArray
+        end
+        
+        def responseWithFile
+            responses:
+				'200':
+				  description: Pdf файл полиса ОСАГО
+				  content:
+					application/pdf:
+					  schema:
+						type: string
+						format: binary
+        end
+    end
 end
 
 module Refs
-	class parameterByRef
-		def linkParameterByRef
-			#/catalog/product/{productId}:
-			#get:
-			  #summary: Метод возвращает список продуктов
-			  #parameters:
-			    #- $ref: '#/components/parameters/ProductId'
-		end
-		
-		def PathParameterAsComponent
-		    ### See linkParameterByRef
-			#components:
-			  #parameters:
-			    ParameterName:
-				  name: parameterName
-				    in: path
-				    required: true
-				    schema:
-					  type: integer
-					  description: Описание
-					  example: 1234
-		end
-	end
+    class parameterByRef
+        def linkParameterByRef
+            #/catalog/product/{productId}:
+            #get:
+              #summary: Метод возвращает список продуктов
+              #parameters:
+                #- $ref: '#/components/parameters/ProductId'
+        end
+        
+        def PathParameterAsComponent
+            ### See linkParameterByRef
+            #components:
+              #parameters:
+                ParameterName:
+                  name: parameterName
+                    in: path
+                    required: true
+                    schema:
+                      type: integer
+                      description: Описание
+                      example: 1234
+        end
+    end
 end
